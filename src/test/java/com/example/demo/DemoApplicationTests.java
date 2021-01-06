efecc30c28334504b3c105b8bb790f9af7830c7d
e <wyjdmf_1001@163.com>
2021-01-06 10:17:08 +0800

    初始化项目

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,8 @@
 package com.example.demo;
 
-import com.alibaba.excel.EasyExcel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.File;
 
 @SpringBootTest
 class DemoApplicationTests {
